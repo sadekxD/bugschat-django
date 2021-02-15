fdc3f59a999b3756d51ec9f38968fadb6395d396
unknown <sadekirfan3@gmail.com>
2021-02-15 19:02:22 +0600

    'static'

--- backend/urls.py
+++ backend/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from chat.views import index
@@ -9,4 +11,4 @@ urlpatterns = [
     path("chat/", include("chat.urls")),
     path("rest-auth/", include("rest_auth.urls")),
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
